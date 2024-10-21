ac4b63838b4842f0873e20c63edf47b85d250c38
Nirfanto Hutuji <programmerabangbentor@gmail.com>
2024-10-21 11:31:24 +0800

    manajement akun

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('title', 'User Forms')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>User Forms</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Forms</a></div>
+                    <div class="breadcrumb-item">Users</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+                <h2 class="section-title">Users</h2>
+
+
+
+                <div class="card">
+                    <form action="{{ route('user.update' , $user) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="card-header">
+                            <h4>Input Text</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" name="name" class="form-control" value="{{ old('name', $user->name) }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" name="email" class="form-control" value="{{ old('email', $user->email) }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">
+                                            <i class="fas fa-lock"></i>
+                                        </div>
+                                    </div>
+                                    <input type="password"
+                                        class="form-control @error('password')
+                                is-invalid
+                            @enderror"
+                                        name="password">
+                                </div>
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Phone</label>
+                                <input type="text" name="phone" class="form-control" value="{{ old('phone', $user->phone) }}">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Roles</label>
+                                <div class="selectgroup w-100">
+                                    <label class="selectgroup-item">
+                                        <input type="radio" name="roles" value="ADMIN" {{ $user->role == 'admin' ? 'selected' : '' }} class="selectgroup-input"
+                                            checked="" >
+                                        <span class="selectgroup-button">Admin</span>
+                                    </label>
+                                    <label class="selectgroup-item">
+                                        <input type="radio" name="roles" value="STAFF" {{ $user->role == 'dosen' ? 'selected' : '' }} class="selectgroup-input">
+                                        <span class="selectgroup-button">Staff</span>
+                                    </label>
+                                    <label class="selectgroup-item">
+                                        <input type="radio" name="roles" value="USER" {{ $user->role == 'mahasiswa' ? 'selected' : '' }} class="selectgroup-input">
+                                        <span class="selectgroup-button">User</span>
+                                    </label>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-primary">Update</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('auth.auth-login');
@@ -15,4 +16,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('home', function () {
         return view('pages.dashboard', ['type_menu' => 'home']);
     })->name('home');
+
+    Route::resource('user', UserController::class);
+
 });
